d328e76918fab57586e5da81550efb3c43b3a4e5
abhinav.vadlamudi <abhinav.vadlamudi@gmail.com>
2024-06-04 18:55:11 +0000

    Added Experience, Skills, Education, Bug Fixes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { SkillsComponent } from './skills/skills.component';
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'certifications', component: CertificationsComponent },
+    // { path: 'certifications', component: CertificationsComponent },
     { path: 'education', component: EducationComponent },
     { path: 'experience', component: ExperienceComponent },
     { path: 'projects', component: ProjectsComponent },
